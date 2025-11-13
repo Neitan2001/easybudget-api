74c612e3724eb7c81c0027c2779e5b0572f07fff
Natan Santana <natan.santana.dev@gmail.com>
2025-11-13 11:04:54 -0300

    feat: setup project with spring boot 3 and Java 17

---
+++ easybudget-api/src/main/java/com/easybudget/easybudget_api/EasybudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.easybudget.easybudget_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybudgetApiApplication.class, args);
+	}
+
+}
